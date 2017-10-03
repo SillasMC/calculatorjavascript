a5f16215902121d10af702c8255679e60442bdca
Sillas Camara <sillasmc@gmail.com>
2017-10-03 14:48:53 -0300

    Operators and CE functions

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     let displayMaxSize = 11;
     let displayPreviewMaxSize = 27;
     let dot = '.';
+    let patt = new RegExp("[^0-9.]");
 
     // Add value to display
     function displayAdd (display, value, maxLength, displayID) {
@@ -52,6 +53,9 @@ $(document).ready(function() {
             return;
         }
 
+        if (patt.test(display))
+            display = '0';
+
         // Add number to display
         if (!displayAdd(display, number, displayMaxSize, "#calc-display-id")){
             console.error(digitLimitError);
@@ -72,24 +76,24 @@ $(document).ready(function() {
         let operation = $("#" + this.id).text();
         let display = $("#calc-display-id").text();
         let displayPreview = $("#calc-display-preview-id").text();
-        let patt = new RegExp("[x+-]");
 
-        if (display.length === 1 && display === '0') {
-            return;
-        }
-
-        console.log(patt.test(display));
-        if (patt.test(display) || display.indexOf("\xF7") >= 0)
+        if ((display.length === 1 && display === '0') || patt.test(display))
             return;
 
         // Add operator to display
         $("#calc-display-id").text(operation);
-/*
+
         // Add number to display preview
-        if (!displayAdd(displayPreview, operation, displayPreviewMaxSize, "#calc-display-preview-id")){
-            console.error(digitLimitError);
+        displayPreview += operation;
+
+        if(displayPreview.length > displayPreviewMaxSize){
+            $("#calc-display-id").text('0');
+            $("#calc-display-preview-id").text(digitLimitError);
+
             return;
-        }*/
+        }
+
+        $("#calc-display-preview-id").text(displayPreview);
 
     });
 
@@ -103,6 +107,16 @@ $(document).ready(function() {
         $("#calc-display-id").text('0');
 
         clearError();
+
+        let displayPreview = $("#calc-display-preview-id").text();
+
+        if (displayPreview != 0){
+            let array = displayPreview.split(patt);
+
+            displayPreview = displayPreview.slice(0, displayPreview.length - array[array.length - 1].length);
+
+            $("#calc-display-preview-id").text(displayPreview);
+        }
     });
 
     // Dot button action
